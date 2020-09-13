20594eb98f2664fdf11b0518ad8b35dbdfa74481
carolinacraus <carocraus@gmail.com>
2020-09-13 16:41:40 -0500

    Update challenge SQL

--- Queries/Employee_Database_challenge.sql .sql
+++ Queries/Employee_Database_challenge.sql .sql
@@ -12,3 +12,15 @@ JOIN titles as ti
 ON (e.emp_no = ti.emp_no) 
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles 
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, 
+	rt.to_date DESC
+
+    
